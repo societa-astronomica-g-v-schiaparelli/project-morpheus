4eb4d1bac785b61dec98de35022831d87a917481
Diego Belloni <diego.belloni@supsi.ch>
2026-06-30 17:55:09 +0000

    feat: add a basic observation script generator

--- backend/script_generator.py
+++ backend/script_generator.py
@@ -1,3 +1,5 @@
+import textwrap
+
 class IndigoScriptGenerator:
     """
     Generatore di script ECMAScript per il controllo di INDIGO Astronomy.
@@ -10,13 +12,27 @@ class IndigoScriptGenerator:
         """Accende i sistemi a inizio nottata."""
         return f"sequence.enable_cooler({self.COOLING_TEMP});"
 
-    def generate_observation(self, target_name: str, ra: str, dec: str) -> str:
-        """Muove il telescopio, aspetta la cupola e avvia la guida."""
-        return f"""
-            sequence.slew("{ra}", "{dec}");
-            sequence.wait({self.DOME_WAIT});
-            sequence.start_guiding({self.FOCUS_EXP});
-        """
+    def generate_observation(self, target_name: str, ra: str, dec: str, frames: int, exposition: float, filter: str, mode: str, guide: bool = False, focus: bool = False) -> str:
+        """Muove il telescopio e gestisce tutti i parametri di osservazione"""
+        script = textwrap.dedent(f"""
+                sequence.set_object_name("{target_name}");
+                sequence.select_camera_mode("{mode}");
+                sequence.select_filter("{filter}");
+                sequence.slew("{ra}", "{dec}");
+                sequence.wait({self.DOME_WAIT});
+                sequence.precise_goto({self.FOCUS_EXP},{ra},{dec});
+            """)
+
+        if focus:
+            script += f"sequence.focus({self.FOCUS_EXP});\n"
+
+        if guide:
+            script += f"sequence.start_guiding({self.FOCUS_EXP});\n"
+
+        script += f"sequence.capture_batch({frames},{exposition});\n"
+        
+        return script
+
 
     def generate_standby(self) -> str:
         """Mette in sicurezza il telescopio durante i buchi temporali."""
@@ -33,5 +49,5 @@ if __name__ == "__main__":
     generator = IndigoScriptGenerator()
     
     print("--- TEST OSSERVAZIONE ---")
-    script = generator.generate_observation("Orione", "05h 35m", "-05d 23m")
+    script = generator.generate_observation("Orione", "05h 35m", "-05d 23m", 4, 30, "R", "RAW 16 1124x899", True)
     print(script)
